353122d0f74b50c0465b4efae7afc75b4eb7c86c
kitsunehanabi <91914131+kitsunehanabi@users.noreply.github.com>
2021-10-10 19:58:35 +0300

    Added lab 1

---
+++ Lab1/Labwork 1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 1. Markdown cells
+#    * trial line 1
+#    * trial line 2
+#    * [referense line](https://www.google.com.ua/?hl=ru)
+
+# # Heading1
+# 
+# ## Heading2
+# 
+# Quoted text: 
+# >So, what about methods?
+
+# Of corse somthing intereste. Math part:
+# $$ y_{sub} = \sqrt(x_{sub}) $$
+
+# Python code:
+
+# In[10]:
+
+
+import math
+def foo():
+    return foo
+
